52937f6b0862901e31a97051cc6e62ed44502510
Muhammed Nabhan <muhammednabhan2301@gmail.com>
2024-01-20 15:49:12 +0530

    styling added

--- src/Signup.js
+++ src/Signup.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './signup.css'
+
 function Signup() {
+ const [formData,setFormData]=useState({
+  firstName:''
+ })
+
   return (
     <div className='signup-card'>
       <h1>Registration form</h1>
       <form>
         <label htmlFor=''>First Name</label>
-        <input type='text' placeholder='first name'/>
+        <input type='text' placeholder='first name' name="firstName"/>
         <label htmlFor=''>Last Name</label>
-        <input type='text' placeholder='Last name'/>
+        <input type='text' placeholder='Last name' name="lastName" />
         <label htmlFor=''>Email</label>
-        <input type='text' placeholder='Email'/>
+        <input type='text' placeholder='Email' name="email"/>
         <label htmlFor=''>Password</label>
-        <input type='text' placeholder='Password'/>
+        <input type='password' placeholder='Password' name="password" />
+        <button>Sign Up</button>
       </form>
+
     </div>
   )
 }
