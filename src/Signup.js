2299b8b1f8d868b0d078999922d8f7ceda3f8e74
Muhammed Nabhan <muhammednabhan2301@gmail.com>
2024-01-20 15:31:34 +0530

    basisc structure done

--- src/Signup.js
+++ src/Signup.js
@@ -4,6 +4,16 @@ function Signup() {
   return (
     <div className='signup-card'>
       <h1>Registration form</h1>
+      <form>
+        <label htmlFor=''>First Name</label>
+        <input type='text' placeholder='first name'/>
+        <label htmlFor=''>Last Name</label>
+        <input type='text' placeholder='Last name'/>
+        <label htmlFor=''>Email</label>
+        <input type='text' placeholder='Email'/>
+        <label htmlFor=''>Password</label>
+        <input type='text' placeholder='Password'/>
+      </form>
     </div>
   )
 }
