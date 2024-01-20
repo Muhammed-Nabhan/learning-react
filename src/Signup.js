5128ea3ba5f52da2e3d692a02d1b7c5ff423aaaf
Muhammed Nabhan <muhammednabhan2301@gmail.com>
2024-01-20 21:03:20 +0530

    for publishing

--- src/Signup.js
+++ src/Signup.js
@@ -3,22 +3,34 @@ import './signup.css'
 
 function Signup() {
  const [formData,setFormData]=useState({
-  firstName:''
+  firstName:'',
+  lastName:'',
+  email:'',
+  password:''
  })
 
+ const handleInputChange=(event)=>{
+const{name,value} = event.target
+setFormData((prevData)=>({...prevData,[name]:value}))
+ }
+
+ const handleSubmit =(event)=>{
+  event.preventDefault();
+  console.log(formData,"The input data is ")
+ }
   return (
     <div className='signup-card'>
       <h1>Registration form</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor=''>First Name</label>
-        <input type='text' placeholder='first name' name="firstName"/>
+        <input type='text' placeholder='first name' name="firstName" value={formData.firstName} onChange={(event)=>handleInputChange(event)}/>
         <label htmlFor=''>Last Name</label>
-        <input type='text' placeholder='Last name' name="lastName" />
+        <input type='text' placeholder='Last name' name="lastName" value={formData.lastName} onChange={(event)=>handleInputChange(event)} />
         <label htmlFor=''>Email</label>
-        <input type='text' placeholder='Email' name="email"/>
+        <input type='email' placeholder='Email' name="email" value={formData.email} onChange={(event)=>handleInputChange(event)}/>
         <label htmlFor=''>Password</label>
-        <input type='password' placeholder='Password' name="password" />
-        <button>Sign Up</button>
+        <input type='password' placeholder='Password' name="password" value={formData.password} onChange={(event)=>handleInputChange(event)} />
+        <button type='submit' >Sign Up </button>
       </form>
 
     </div>
